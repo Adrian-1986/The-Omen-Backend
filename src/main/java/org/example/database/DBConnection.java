8dda99c5e4dafc5764ab3a7300f181364b8285d3
Adrian-1986 <adryhonor9@gmail.com>
2026-03-24 14:17:49 +0100

    Load DB credentials from local properties

--- src/main/java/org/example/database/DBConnection.java
+++ src/main/java/org/example/database/DBConnection.java
@@ -1,15 +1,29 @@
 package org.example.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 // Provides a single place to manage database connections.
 public class DBConnection {
     public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/videoclub?useSSL=false&serverTimezone=UTC";
-        String user = "root";
-        String password = "s1expres"; // <-- importante
+        String user = "";
+        String password = "";
+
+        Properties props = new Properties();
+        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input != null) {
+                props.load(input);
+                user = props.getProperty("db.user", "");
+                password = props.getProperty("db.password", "");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try {
             System.out.println("Intentando conectar a: " + url);
